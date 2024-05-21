ae726284b18d353ab37c1552f81ef4db6c219131
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-05-21 11:32:26 -0300

    metodo de alterar senha

--- SistemaDeCadastro/obj/Debug/net8.0/SistemaDeCadastro.AssemblyInfo.cs
+++ SistemaDeCadastro/obj/Debug/net8.0/SistemaDeCadastro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeCadastro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b743ca6729aa853c31e357da2859c05b8787142e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77ec113efbd29ef1890e6daffcb9a571e6ae8c6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeCadastro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeCadastro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
