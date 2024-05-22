13d098fa68dbc784abd55c11db2bbb673ca36638
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-05-22 18:00:40 -0300

    dll

--- SistemaDeCadastro/obj/Debug/net8.0/SistemaDeCadastro.AssemblyInfo.cs
+++ SistemaDeCadastro/obj/Debug/net8.0/SistemaDeCadastro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeCadastro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ded8b790f2a885daaaeed0b5c8e0d36cc13497c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71bd810397bd5ccc957222ad80dfcda0c325e0d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeCadastro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeCadastro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
