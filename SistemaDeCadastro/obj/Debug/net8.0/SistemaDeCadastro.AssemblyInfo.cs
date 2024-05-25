a2cf643a5ca54c00ac5ef2c8e1bd923bc27fdec8
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-05-25 11:26:07 -0300

    delete startup.cs

--- SistemaDeCadastro/obj/Debug/net8.0/SistemaDeCadastro.AssemblyInfo.cs
+++ SistemaDeCadastro/obj/Debug/net8.0/SistemaDeCadastro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeCadastro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5fef4f9e85676c8904f4375903d2ab598f44322")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ba93a3ac8f5d811ed6aaf1bea51f6612510b3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeCadastro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeCadastro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
